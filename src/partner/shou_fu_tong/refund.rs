7c41c4173709b325e1941d210c7acc28369956d5
sjava <515957966@qq.com>
2025-03-25 16:36:29 +0800

    修改收付通退款解密消息中success_time定义

--- src/partner/shou_fu_tong/refund.rs
+++ src/partner/shou_fu_tong/refund.rs
@@ -1,6 +1,8 @@
 pub mod refund_apply;
 pub mod refund_query;
 
+use crate::util::option_datetime_fmt;
+use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -12,8 +14,12 @@ pub struct RefundNotifyData {
     pub refund_id: String,
     pub out_refund_no: String,
     pub refund_status: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub success_time: Option<String>,
+    #[serde(
+        with = "option_datetime_fmt",
+        skip_serializing_if = "Option::is_none",
+        default
+    )]
+    pub success_time: Option<DateTime<Local>>,
     pub user_received_account: String,
     pub amount: RefundAmount,
     #[serde(skip_serializing_if = "Option::is_none")]
